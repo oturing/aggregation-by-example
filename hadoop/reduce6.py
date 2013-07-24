a45d51a3ab65aa8cbfa43fbd660bdc4b1799bc09
Luciano Ramalho <luciano@ramalho.org>
2013-07-24 08:49:46 -0700

    the shortest so far

---
+++ hadoop/reduce6.py
@@ -0,0 +1,9 @@
+import sys
+from itertools import groupby
+from operator import itemgetter
+
+split_stdin = (item.split() for item in sys.stdin)
+
+for key, group in groupby(split_stdin, itemgetter(0)):
+    count = sum(1 for pair in group)
+    print '%s\t%i' % (key, count)
