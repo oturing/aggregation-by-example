32829c384631887abb15705953175f86e07ed15f
Luciano Ramalho <luciano@ramalho.org>
2013-07-24 08:50:31 -0700

    the shortest so far

--- hadoop/reduce6.py
+++ hadoop/reduce6.py
@@ -5,5 +5,5 @@ from operator import itemgetter
 split_stdin = (item.split() for item in sys.stdin)
 
 for key, group in groupby(split_stdin, itemgetter(0)):
-    count = sum(1 for pair in group)
+    count = sum(1 for item in group)
     print '%s\t%i' % (key, count)
